2a9c1ba9e6c848de7d99df1973221b8f53dbfe56
Jackie Zou <jackiezou123@yahoo.com>
2026-02-23 21:08:44 -0500

    Add Inventory class to manage item storage and equipment
    - Implemented an Inventory class to handle adding, removing, and equipping items.
    - Introduced methods for inventory management, including checks for duplicates and available slots.
    - Established data structures for tracking equipped items and inventory state.

---
+++ src/Inventory.js
@@ -0,0 +1,105 @@
+export default class Inventory {
+  constructor(scene) {
+        this.itemSetForDupeCheckInventory = new Set();
+        this.itemSetForDupeCheckEquipped = new Set();
+        this.itemsInInventory = 0;
+        this.INVENTORYSIZE = 10;
+
+        this.inventory = new Map();
+        this.equipped = new Map();
+
+        this.equipped.set('head', null);
+        this.equipped.set('body', null);
+        this.equipped.set('bottom', null);
+        this.equipped.set('feet', null);
+        this.equipped.set('weapon', null);
+        this.equipped.set('offhand', null);
+
+        this.revertedInventoryMap = new Map();
+        this.emptySlots = new Set([1,2,3,4,5,6,7,8,9,10]);
+        
+    }
+    
+
+    _addItemToInventory(item) {
+        //if there is no more room in inventory return false
+        if(this.itemsInInventory >= this.INVENTORYSIZE){
+            return false;
+        }
+        //if inventory already contains the item return false
+        else if(this.itemSetForDupeCheckInventory.has(item.id)){
+            return false;
+        }
+        else{ //sucess 
+            const slot = this.emptySlots.values().next().value;
+            this.emptySlots.delete(slot);
+            this.inventory.set(slot, item);
+            this.revertedInventoryMap.set(item.id, slot);
+            this.itemSetForDupeCheckInventory.add(item.id);
+            this.itemsInInventory++;
+        }
+    }
+    _removeItemFromInventory(item){
+        if(this.itemsInInventory===0){
+            return false;
+        }
+        else if(!this.itemSetForDupeCheckInventory.has(item.id)){
+            return false;
+        }
+        else{
+            const slot = this.revertedInventoryMap.get(item.id);
+            this.inventory.delete(slot);
+            this.revertedInventoryMap.delete(item.id);
+            this.itemSetForDupeCheckInventory.delete(item.id);
+            this.emptySlots.add(slot);
+            this.itemsInInventory--;
+        }
+        
+    }
+    _equipItemFromInventory(item){
+        this.slot = item.slot;
+        if(this.itemsInInventory===0){
+            return false;
+        }
+        //if item is not in inventory return false
+        else if(!this.itemSetForDupeCheckInventory.has(item.id)){
+            return false;
+        }
+        else{//equip item, remove from inventory, add to equipped
+            const slot = this.revertedInventoryMap.get(item.id);
+            this.equipped.set(this.slot, item);
+            this.itemSetForDupeCheckEquipped.add(item.id);
+            this.itemSetForDupeCheckInventory.delete(item.id);
+            this.inventory.delete(slot);
+            this.revertedInventoryMap.delete(item.id);
+            this.emptySlots.add(slot);
+            this.itemsInInventory--;           
+        }
+        
+
+    }
+    _removeItemFromEquipped(item){
+        this.slot = item.slot;
+        //if equipped is empty return false
+        if(this.itemSetForDupeCheckEquipped.size===0){
+            return false;
+        }
+        //if item is not in equipped
+        else if(!this.itemSetForDupeCheckEquipped.has(item.id)){
+            return false;
+        }
+        else{
+            const slot = this.emptySlots.values().next().value;
+            this.emptySlots.delete(slot);
+            this.equipped.set(this.slot, null);
+            this.itemSetForDupeCheckEquipped.delete(item.id);
+            this.inventory.set(slot, item);
+            this.revertedInventoryMap.set(item.id, slot);
+            this.itemSetForDupeCheckInventory.add(item.id);
+            this.itemsInInventory++;
+        }
+            
+    }
+
+
+}
